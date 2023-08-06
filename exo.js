a4b6ba6aacc219af6030145a14c2afc03a753a47
MCoclock <139005658+MCoclock@users.noreply.github.com>
2023-08-07 01:31:04 +0200

    Add files via upload

---
+++ exo.js
@@ -0,0 +1,19 @@
+console.log("test 1...2...1...2");
+
+let Hercule = {firstname: "Hercule", job: "Demi Dieu", age: "35 ans", departement: "75", arm: "60.5", inRelationShip: "Oui"}
+
+console.log(Hercule);
+
+base.fillProfil(Hercule);
+
+
+let amis = [
+    {firstname: "Jupiter", job: "Demi Dieu", age: "35 ans", departement: "75"},
+    {firstname: "Junon", job: "Demi Dieu", age: "35 ans", departement: "75"},
+    {firstname: "Alcmène", job: "Demi Dieu", age: "35 ans", departement: "75"},
+    {firstname: "Déjanire", job: "Demi Dieu", age: "35 ans", departement: "75"}
+]
+
+console.log(printfriends);
+
+h1 = banner__title("consulter le profil de Hercule")
